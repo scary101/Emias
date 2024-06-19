10e891b2a6898ce09027d9a1aad7f4da5e20d1cc
pepegajk <alekseje2086@gmail.com>
2024-06-19 21:50:52 +0300

    asdasd

---
+++ Emias/View/profile.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Emias.View
+{
+    /// <summary>
+    /// Логика взаимодействия для profile.xaml
+    /// </summary>
+    public partial class profile : Page
+    {
+        public profile()
+        {
+            InitializeComponent();
+        }
+
+        private void namepolis_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void mobilephone_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void UserControl1_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
